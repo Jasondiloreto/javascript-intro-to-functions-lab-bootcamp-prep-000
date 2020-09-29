3c510823d1085952f6ffd51c94b8ca67dfd24e32
Jason DiLoreto <jasondiloreto@yahoo.com>
2020-09-29 22:36:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,15 +14,23 @@ function logWhisper(string) {
   console.log(`hello`)
 }
 
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
-  }
+var uppercase = "HELLO"
+uppercase.toUpperCase() === uppercase
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase
+var mixedCase = 'Hi there!'
+mixedCase.toLowerCase() === mixedCase
+mixedCase.toUpperCase() === mixedCase
 
-  if (string.toUpperCase() === string) {
-    return "YES INDEED!"
-  }
-
-  if (string === "I love you, Grandma.") {
-    return "I love you, too."
-  }
+function sayHitoGrandma(string) {
+if (lowercase === "hello") {
+  console.log("I can't hear you!")
+  return string.toUpperCase(`I can't hear you!`)
+} else if (uppercase === "HELLO") {
+  console.log("YES INDEED!")
+  return string.toLowerCase(`YES INDEED!`)
+} else if (mixedcase === "I love you, Grandma") {
+  console.log("I love you, too.") 
+  return string.toMixedCase(`I love you, too.`)
+}
+}
